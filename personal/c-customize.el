41a3f46a06c1f43cfc970753d29d9e7640ed0237
Tim Woods <tim@thewoods.info>
2013-08-18 19:36:12 -0400

    Add c-eldoc

--- personal/c-customize.el
+++ personal/c-customize.el
@@ -17,3 +17,39 @@
   (define-key c++-mode-map (kbd "RET") 'c-context-line-break))
 
 (add-hook 'c-initialization-hook 'my-c-initialization-hook)
+
+
+;;; Enable c-eldoc
+(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+
+
+;;; c-eldoc add include directories helper
+;;; Based on https://github.com/brianjcj/auto-complete-clang/issues/20
+(defun add-my-include-directories ()
+  (interactive)
+  (setq c-eldoc-includes
+        (mapconcat #'(lambda (dir) (concat "-I" dir))
+                   my-include-directories
+                   " ")))
+
+
+;;; Add include directories for c-eldoc - call from .dir-locals.el
+(defun add-project-directories (&rest args)
+  (defvar my-include-directories nil)
+  (make-variable-buffer-local 'my-include-directories)
+  (setq my-include-directories
+        (mapcar (lambda (subdir)
+                  (if (eq (elt subdir 0) ?/)
+                      subdir
+                    (concat (file-name-directory
+                             (file-truename (locate-dominating-file
+                                             buffer-file-name
+                                             ".dir-locals.el")))
+                            subdir)))
+                args))
+  (add-my-include-directories))
+
+;;; Example: Add the following lines to .dir-locals.el
+;; ((c-mode . ((eval . (add-project-directories "include/path/1/"
+;;                                              "include/path/2/"
+;;                                              "include/path/3/")))))
