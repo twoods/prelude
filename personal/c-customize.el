10cdd26c8ee382308ca4e7939716e373096274b9
Tim Woods <tim@thewoods.info>
2013-08-18 19:15:10 -0400

    Use C context line break in C and C++

--- personal/c-customize.el
+++ personal/c-customize.el
@@ -8,3 +8,12 @@
   (setq indent-tabs-mode nil))
 
 (add-hook 'prelude-c-mode-common-hook 'my-prelude-c-mode-defaults t)
+
+
+;;; Customize C keymap
+(defun my-c-initialization-hook ()
+  ;;; Use C specific line break on RETURN key
+  (define-key c-mode-base-map (kbd "RET") 'c-context-line-break)
+  (define-key c++-mode-map (kbd "RET") 'c-context-line-break))
+
+(add-hook 'c-initialization-hook 'my-c-initialization-hook)
