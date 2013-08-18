24c0183fc92c81372a6061d34113b6175af2e0f7
Tim Woods <tim@thewoods.info>
2013-08-18 19:13:09 -0400

    Disable indent tabs mode for C

--- personal/c-customize.el
+++ personal/c-customize.el
@@ -2,3 +2,9 @@
 
 (setq c-default-style "bsd"
       c-basic-offset 4)
+
+;;; Use spaces instead of tabs
+(defun my-prelude-c-mode-defaults ()
+  (setq indent-tabs-mode nil))
+
+(add-hook 'prelude-c-mode-common-hook 'my-prelude-c-mode-defaults t)
